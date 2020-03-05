bd6bc34ed5c18b97466b3d19b0d5573734b13fe8
RenyxOne <Renyxone@gmail.com>
2020-03-05 23:32:11 +0300

    Done

--- Kruskal/Kruskal/Source.cpp
+++ Kruskal/Kruskal/Source.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 using namespace std;
 
-class node {
+class edge {
 private:
 	unsigned char from;
 	unsigned char to;
@@ -18,44 +18,103 @@ public:
 	char To() {
 		return to;
 	}
-	node() {};
-	node(const node& a) {
+	edge() { from = 0; to = 0; w = 0; }
+	~edge() { from = 0; to = 0; w = 0; }
+	edge(const edge& a) {
 		from = a.from;
 		to = a.to;
 		w = a.w;
 	}
-	node(char from, char to, int w) {
+	edge(char from, char to, int w) {
 		this->from = from;
 		this->to = to;
 		this->w = w;
 	}
-	void swap(node& a) {
-		node temp(*this);
+	void swap(edge& a) {
+		edge temp(*this);
 		*this = a;
 		a = temp;
 	}
-	node operator = (const node& a) {
+	edge operator = (const edge& a) {
 		from = a.from;
 		to = a.to;
 		w = a.w;
 		return *this;
 	}
-	bool operator > (const node& a) {
+	bool operator > (const edge& a) {
 		return (w > a.w);
 	}
-	bool operator < (const node& a) {
+	bool operator < (const edge& a) {
 		return (w < a.w);
 	}
-	friend ostream& operator<< (ostream& os, node& a) {
+	friend ostream& operator<< (ostream& os, edge& a) {
 		return os << '(' << a.from << ',' << a.to << ',' << a.w << ')';
 	}
-	friend istream& operator>> (istream& is, node& a)
+	friend istream& operator>> (istream& is, edge& a)
 	{
 		return is >> a.from >> a.to >> a.w;
 	}
 };
+class graph {
+public:
+	vector<vector<int>> Graph;
+	vector<char> V;
 
-void BubbleSort(vector<node>& E) {
+	graph() { Graph.resize(26); }
+	graph(vector<edge>& E) {
+		for (int i = 0; i < E.size(); i++)
+			*this = *this + E[i];
+		getVersh();
+	}
+	graph(const graph &a) {
+		Graph = a.Graph;
+		V = a.V;
+	}
+	~graph() { Graph.resize(0); V.resize(0); }
+	void getVersh() {
+		vector<char> temp;
+		for (int i = 0; i < Graph.size(); i++)
+			if (!Graph[i].empty()) temp.push_back(i + 'a');
+		V = temp;
+	}
+	char getV() {
+		return V[0];
+	}
+	vector<vector<int>> getGraph() {
+		return Graph;
+	}
+
+	graph doGraph(vector<edge>& E) {
+		graph temp;
+		for (int i = 0; i < E.size(); i++)
+			temp = temp + E[i];
+		temp.getVersh();
+		return temp;
+	}
+	graph operator + (edge& a) {
+		Graph[a.From()-'a'].push_back(a.To()-'a');
+		Graph[a.To()-'a'].push_back(a.From()-'a');
+		getVersh();
+		return *this;
+	}
+	graph& operator = (const graph &a) {
+		Graph = a.Graph;
+		V = a.V;
+		return *this;
+	}
+	friend ostream& operator << (ostream& os, const graph& a) {
+		for (int i : a.V)
+		{
+			os << (char)i << " -> ";
+			for (int j : a.Graph[i-'a'])
+				os << (char)(j+'a')<< ' ';
+			os << endl;
+		}
+		return os;
+	}
+};
+
+void BubbleSort(vector<edge>& E) {
 	bool flag = true;
 	while (flag) {
 		flag = false;
@@ -67,18 +126,16 @@ void BubbleSort(vector<node>& E) {
 	}
 }
 
-vector<vector<int>> Smezh(vector<node>& E)
-{
-	vector<vector<int>> G(26);
-	for (int i = 0; i < E.size(); i++) {
-		G[E[i].From() - 'a'].push_back(E[i].To() - 'a');
-		G[E[i].To() - 'a'].push_back(E[i].From() - 'a');
+bool HaveCycle(graph &G, int U, int prevU, vector<bool> &done) {
+	done[U] = true;
+	for (int v = 0; v < G.Graph[U].size(); v++)
+	{
+		if (!done[G.Graph[U][v]]) {
+			if (HaveCycle(G, G.Graph[U][v], U, done)) return true;
+		}
+		else if (G.Graph[U][v] !=prevU)
+			return true;
 	}
-	return G;
-}
-
-bool HaveCycle(vector<vector<int>> G) {
-	if (1)
 	return false;
 }
 
@@ -86,29 +143,34 @@ int main()
 {
 	int cE;
 
-	cout << "Input |E|" << endl;
+	cout << "Vvedite kolichestvo reber" << endl;
 	cin >> cE;
 
-	vector<vector<int>> G(26);
-	vector<node> E(cE);
+	vector<edge> E(cE);
 
 	for (int i = 0; i < E.size(); i++)
 		cin >> E[i];
 
-	G = Smezh(E);
-
-	vector<char> V;
-	for (int i = 0; i < G.size(); i++)
-		if (!G[i].empty()) V.push_back(i + 'a');
-
-	/*cout << "Graph sostoit iz vershin" << endl;
-	for (int i = 0; i < V.size(); i++)
-		cout << V[i] << ' ';
-	cout << endl;*/
+	graph G;
 
 	//Сортируем по весу
 	BubbleSort(E);
+	vector<edge> T;
+	//Подбираем ребра
+	for (int i = 0; i < E.size(); i++) {
+		vector<bool> temp(26, 0);
+		graph tG = G + E[i];
+		if (!HaveCycle(tG, E[0].From()-'a', -1, temp)) {
+			G = tG;
+			T.push_back(E[i]);
+		}
+	}
+	cout << "Minimalnoe ostavnoe derevo" << endl;
 
-	for (int i = 0; i < E.size(); i++)
-		cout << E[i] << endl;
+	//Вывод ребер
+	/*for (int i = 0; i < T.size(); i++)
+		cout << T[i] << endl;*/
+
+	//Вывод списка смежности
+	cout << G;
 }
