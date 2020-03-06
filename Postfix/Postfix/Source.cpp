8f8755c562438ec941a1fc6302969428b442c94d
RenyxOne <Renyxone@gmail.com>
2020-03-06 19:55:46 +0300

    AddPostfix

---
+++ Postfix/Postfix/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class versh {
+public:
+    char name;
+    int son;
+    bool Used;
+    vector<char> Sons;
+    versh() { name = 0; son = 0; Used = false; Sons.resize(0); }
+    friend istream& operator >> (istream& is, versh& a) {
+        return is >> a.name >> a.son;
+    }
+    friend ostream& operator << (ostream& os, versh& a) {
+        for (char i : a.Sons)
+            os << i << ' ';
+        return os;
+    }
+    void used() { Used = true; }
+    bool isUsed() { return Used; }
+    void addSon(versh& a) { Sons.push_back(a.name); son -= 1; a.used(); }
+};
+
+int main()
+{
+    int count;
+    cin >> count;
+    vector<versh> G(count);
+    for (int i = 0; i < count; i++)
+        cin >> G[i];
+
+    for (int i = 0; i < G.size(); i++)
+    {
+        if (G[i].son != 0) {
+            int l = i - 1;
+            while (G[i].son) { if (!G[l].isUsed() && l < G.size()) { G[i].addSon(G[l]); } l -= 1; }
+        }
+    }
+    for (versh i : G)
+        if (!(i.Sons.empty())) cout << i.name << " -> " << i << endl;
+}
